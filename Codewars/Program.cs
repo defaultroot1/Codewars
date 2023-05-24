7842361b59a5918ac96693408ab70aa1c8e3d2d8
u01 <u01@CORE>
2023-05-24 14:56:14 +0100

    Add project files.

---
+++ Codewars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Codewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.CountPositivesSumNegatives(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }));
+            
+        }
+    }
+}

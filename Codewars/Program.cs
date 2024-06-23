5ed5158fb719a5380567469138bb2c9891a9a406
defaultroot1 <mailniallc@gmail.com>
2024-06-23 19:19:46 +0100

    Number of People in the Bus

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -7,14 +7,34 @@ namespace Codewars
         static void Main(string[] args)
         {
             // Tests here
-            Console.WriteLine(Kata.IsEven(20));
-			Console.WriteLine(Kata.IsEven(23));
-			Console.WriteLine(Kata.IsEven(0.5));
-			Console.WriteLine(Kata.IsEven(-4));
-		}
 
-        public class Kata
+            List<int[]> peopleList1 = new List<int[]>() { new[] { 10, 0 }, new[] { 3, 5 }, new[] { 5, 8 } };
+			List<int[]> peopleList2 = new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 10 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 10 } };
+			List<int[]> peopleList3 = new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 8 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 8 } };
+
+            Console.WriteLine(Kata.Number(peopleList1));
+            Console.WriteLine(Kata.Number(peopleList2));
+            Console.WriteLine(Kata.Number(peopleList3));
+
+
+
+
+        }
+
+		public class Kata
         {
+			public static int Number(List<int[]> peopleListInOut)
+			{
+                int numOnBus = 0;
+
+                foreach (int[] inOutPair in peopleListInOut)
+                {
+                    numOnBus = numOnBus + inOutPair[0] - inOutPair[1];
+                }
+
+                return numOnBus;
+			}
+
 			public static bool IsEven(double n)
 			{
                 return (n % 2 == 0);
